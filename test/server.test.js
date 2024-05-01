433a1043aae580cd03b6de00b5d76a1afaae42da
kuroko2860 <developer2860@gmail.com>
2024-05-02 01:16:24 +0700

    test cd

--- test/server.test.js
+++ test/server.test.js
@@ -1,5 +1,5 @@
-const request = require("supertest");
-const app = require("../server");
+// const request = require("supertest");
+// const app = require("../server");
 
 // mock test
 function sum(a, b) {
